ba7f735ea03058fda708e4739b7b1b49f3882f88
Venky Iyer <indigoviolet@gmail.com>
2018-10-10 16:36:27 -0700

    Add dmpypy

---
+++ flycheck-dmypy.el
@@ -0,0 +1,60 @@
+;;; flycheck-dmypy.el --- Support dmypy in flycheck
+
+
+;;; Commentary:
+
+;; This package adds support for dmypy to flycheck.  To use it, add
+;; to your init.el:
+
+;; (require 'flycheck-dmypy)
+;; (add-hook 'python-mode-hook 'flycheck-mode)
+
+
+;;; License:
+
+;; This file is not part of GNU Emacs.
+;; However, it is distributed under the same license.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+(require 'flycheck)
+
+(flycheck-def-args-var flycheck-python-dmypy-args python-dmypy)
+
+(flycheck-define-checker python-dmypy
+  "Dmypy syntax checker.
+
+Customize `flycheck-python-dmypy-args` to add specific args to default
+executable.
+
+E.g. when processing Python2 files, add \"--py2\".
+
+See URL `http://mypy-lang.org/'."
+
+  :command ("dmypy"
+            (eval flycheck-python-dmypy-args)
+            source-original)
+  :error-patterns
+  ((error line-start (file-name) ":" line ": error:" (message) line-end)
+   (warning line-start (file-name) ":" line ": note:" (message) line-end)
+   (info line-start (file-name) ":" line ": note:" (message) line-end))
+  :modes python-mode)
+
+(add-to-list 'flycheck-checkers 'python-dmypy t)
+
+(provide 'flycheck-dmypy)
+;;; flycheck-dmypy.el ends here
